46e3d3f6a4deee044a41bedabc38f0a176922cf4
Abel-Lagonell <alagonell1730@floridapoly.edu>
2022-01-16 17:52:17 -0500

    Create Abel Lagonell - HW 1 - Task 1.c

---
+++ Abel Lagonell - HW 1 - Task 1.c
@@ -0,0 +1,20 @@
+/**
+ * Title:               Hello World!
+ * Author:              Abel Lagonell
+ * Email:               alagonell1730@floridapoly.edu
+ * Version:             0.0.1a
+ * Creation Date:       01/13/2022 @ 5:39 PM
+ * Modification Date:   01/13/2022 @ 5:39 PM
+ * Description:         This is the first program in C. The code prints "Hello, World!" on the screen.
+*/
+
+//Header Files
+#include <stdio.h>
+
+//main function is the entry point to the program
+int main(int argc, char *argv[]){
+    // printf is a function in the standard library I/O header, and it prints the values on the standard output.
+    printf("Hello, World!\n");
+
+    return 0;   //Inform the operating sustem that the exit is succesfull
+}//The end of the main function
