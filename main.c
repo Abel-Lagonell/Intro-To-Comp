59ad2f674ed984e4d982e581b6db13397be9f10f
Abel-Lagonell <alagonell1730@floridapoly.edu>
2022-01-17 09:47:36 -0500

    task 2 complete

---
+++ main.c
@@ -0,0 +1,23 @@
+/****************************************************************
+ * Title:               Printing multiline messages 
+ * Author:              Abel Lagonell
+ * Email:               alagonell@floridapoly.edu
+ * Version:             0.0.1a
+ * Creation date:       1/17/2022 @ 9:46 AM
+ * Modification Date:   1/17/2022 @ 9:46 AM
+ * Description:         Print out a multiline message with the given format and contents.
+ * 
+*****************************************************************/
+
+//Header file
+#include <stdio.h>
+
+//main function is the entry point for the program
+int main(int argc, char *argv[]){
+
+    //The printf function that prints out the multiline message
+    printf("Florida Polytechnic University\nStudent ID Card\n\nName: John Smith\nCredit Hours: 22\nGPA: 1.67");
+
+    return 0;//Inform the operating system that exit is successful
+}// The end of the main function
+
