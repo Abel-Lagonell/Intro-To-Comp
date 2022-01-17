0644d5222c7b4020527dc107f64c2e57e1c2dfea
Abel-Lagonell <alagonell1730@floridapoly.edu>
2022-01-17 09:19:09 -0500

    Finished task

--- Task 1.c
+++ Task 1.c
@@ -1,8 +1,21 @@
+/**
+ * Title                Hello World!
+ * Author               Abel Lagonell
+ * Email:               alagonell@floridapoly.edu
+ * Version:             0.0.1a
+ * Creation date:       1/16/2022 @ 11:39 PM
+ * Modification Date:   1/16/2022 @ 11:39 PM
+ * Description:         This is the first program in C. The code prints "Hello, World!" on the screen.
+ */
+
+// Header Files
 #include <stdio.h>
 
+// main function is the entry point to the program
 int main(int argc, char *argv[])
 {
-    printf("Hello, world!");
+    //printf is a function in the standard library I/O header, and it prints the values on the standard output.
+    printf("Hello, world!\n");
 
-    return 0;
-}
+    return 0;//Inform the operating system that the exit is successful.
+}// The end of the main function
